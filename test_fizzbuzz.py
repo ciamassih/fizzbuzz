25494d5330ecd8244518fbad339059c8e5bad6a9
ciamassih <c.abdulmassihlopez1@usp.ceu.es>
2022-01-08 17:41:40 +0100

    Test imprimir un 9 como fizz por ser multiplo de 3 OK

--- test_fizzbuzz.py
+++ test_fizzbuzz.py
@@ -5,6 +5,10 @@ from fizzbuzz import Fizzbuzz
 class TestFizzbuzz(unittest.TestCase):
 
 
-    def test_multiple_of_three(self):
+    def test_multiple_of_three_6(self):
         fizzbuzz = Fizzbuzz()
         self.assertEqual(fizzbuzz.process(6), "Fizz")
+
+    def test_multiple_of_three_9(self):
+        fizzbuzz = Fizzbuzz()
+        self.assertEqual(fizzbuzz.process(9), "Fizz")
